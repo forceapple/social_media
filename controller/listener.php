6ee637b8242a03a949837936a9c79c6cf5278b82
Ilya Ulyanov <ilya.wt@gmail.com>
2015-03-06 11:07:48 -0800

    initial controller

---
+++ controller/listener.php
@@ -0,0 +1,32 @@
+<?php
+require_once('main.php');
+
+// *******************
+// GET PHASES
+// *******************
+// 0 - get all posts
+// 1 - get post by user id
+// 2 - get comments by post id
+
+// TO DO
+// class constructor
+
+if(isset($_GET['phase'])){
+	switch($_GET['phase']){
+		case 0:
+			$lo = new noniController();
+			return json_encode($lo->get_all_post());
+		break;
+		case 1:
+			$lo = new noniController();
+			return json_encode($lo->get_post($_GET['uid']));
+		break;
+		case 2:
+			$lo = new noniController();
+			return json_encode($lo->get_comments($_GET['cid']));
+		break;
+	}
+}
+
+
+?>
---
+++ controller/main.php
@@ -0,0 +1,33 @@
+<?php
+require_once('../model/main.php');
+
+class noniController{
+
+	//function to get all posts from the db
+	//returns an array of posts
+	function get_all_post(){
+		$co = new Noni();
+		return $co->get_all_post();
+	}
+
+	//function to get post by user id
+	//returns a single array
+	function get_post($uid){
+		$co = new Noni();
+		return json_encode($co->get_post($uid));
+	}
+
+	//function to get comments by post id
+	//returns an array of comments
+	function get_comments($pid){
+		$co = new Noni();
+		return json_encode($co->get_comments($pid));
+	}
+
+}
+
+/*
+$test = new noniController();
+print_r($test->get_all_post());
+*/
+?>
