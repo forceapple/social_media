e87e101910c776c86cbb99b67b1019cc2cd4b1b4
annelorraineuy <annelorraineuy@gmail.com>
2015-03-20 14:52:06 -0700

    test

--- model/main.php
+++ model/main.php
@@ -12,13 +12,14 @@ class Noni{
 		
 		global $con;
 		$this->con = $con;
+		//echo $this->con;
 	}
 	//get all post
 	function get_all_post(){
-		global $con;
+		
 		$query = "SELECT * FROM post LEFT JOIN user_post ON user_post.pid = post.pid
 										LEFT JOIN user ON user.uid = user_post.uid";
-		$result = mysqli_query($con, $query);
+		$result = mysqli_query($this->con, $query);
 		if($result){
 			$arr = array();
 			while($row = mysqli_fetch_array($result)){
@@ -34,7 +35,7 @@ class Noni{
 				$arr['profile_img']= $row['profile_img'];
 				$arr['time_stamp']= $row['time_stamp'];
 				$query2="SELECT COUNT(*) FROM comments_posts WHERE comments_posts.pid=".$pid;
-				$result2 = mysqli_query($con, $query2);
+				$result2 = mysqli_query($this->con, $query2);
 				while($row2 = mysqli_fetch_array($result2)){
 					$arr4['count']=$row2['COUNT(*)'];
 					$arr['num_comment']= $arr4['count'];
@@ -48,10 +49,9 @@ class Noni{
 	}
 	//get post by post id
 	function get_post($pid){
-		global $con;
 		$query = "SELECT user.username, user_post.pid,user.profile_img, user_post.uid, post.title, post.post_image, post.type, post.text, post.time_stamp  FROM post LEFT JOIN user_post ON user_post.pid = post.pid
 									LEFT JOIN user ON user.uid = user_post.uid WHERE post.pid=".$pid;
-		$result = mysqli_query($con, $query);
+		$result = mysqli_query($this->con, $query);
 		if($result){
 			$arr = array();
 			while($row = mysqli_fetch_array($result)){
@@ -73,9 +73,8 @@ class Noni{
 	}
 	// get post by uid
 		function get_post_by_uid($uid){
-			global $con;
 			$query = "SELECT user.username, user_post.pid,user.profile_img, user_post.uid, post.title, post.post_image, post.type, post.text FROM post LEFT JOIN user_post ON user_post.pid = post.pid LEFT JOIN user ON user.uid = user_post.uid WHERE user.uid=".$uid;
-			$result = mysqli_query($con, $query);
+			$result = mysqli_query($this->con, $query);
 			if($result){
 				$arr = array();
 				while($row = mysqli_fetch_array($result)){
@@ -97,11 +96,10 @@ class Noni{
 		}	
 	//get comments by post id
 	function get_comments($pid){
-		global $con;
 		$query = "SELECT * FROM post LEFT JOIN comments_posts ON comments_posts.pid = post.pid
 										LEFT JOIN user ON user.uid = comments_posts.uid
 										LEFT JOIN comments ON comments.cid = comments_posts.cid WHERE post.pid=".$pid;
-		$result = mysqli_query($con, $query);
+		$result = mysqli_query($this->con, $query);
 
 		if($result){
 			$arr = array();
@@ -120,31 +118,29 @@ class Noni{
 
 	}
 	function create_post($uid, $title, $text, $url, $type){
-			global $con;
-			$query = "INSERT INTO post(title, text, post_image, type) VALUES ('".$title."','".$text."','".$url."','".$type."')";
-			$result = mysqli_query($con, $query);
-			if($result){
-				//get last insert id and insert into user_post
-				$id = mysqli_insert_id($con);
-				$query = "INSERT INTO user_post(uid,pid) VALUES ('".$uid."','".$id."')";
-				$result2 = mysqli_query($con,$query);
-				if($result2){
-					//both inserts are good, return true
-					return true;
-				}
-				return false;
+		$query = "INSERT INTO post(title, text, post_image, type) VALUES ('".$title."','".$text."','".$url."','".$type."')";
+		$result = mysqli_query($this->con, $query);
+		if($result){
+			//get last insert id and insert into user_post
+			$id = mysqli_insert_id($this->con);
+			$query = "INSERT INTO user_post(uid,pid) VALUES ('".$uid."','".$id."')";
+			$result2 = mysqli_query($this->con,$query);
+			if($result2){
+				//both inserts are good, return true
+				return true;
 			}
 			return false;
+		}
+		return false;
 	}
 
 	function create_comment($uid, $pid, $comment){
-		global $con;
 	 	$query = "INSERT INTO comments(comment) VALUES ('".$comment."')";
-		$result = mysqli_query($con,$query);
+		$result = mysqli_query($this->con,$query);
 		if($result){
-			$id = mysqli_insert_id($con);
+			$id = mysqli_insert_id($this->con);
 			$query = "INSERT INTO comments_posts(pid,uid,cid) VALUES('".$pid."','".$uid."','".$id."')";
-			$result2 = mysqli_query($con, $query);
+			$result2 = mysqli_query($this->con, $query);
 			if($result2){
 				//both inserts are good, return true
 				return true;
@@ -154,13 +150,12 @@ class Noni{
 		return false;
 	}
 	function edit_post($pid,$uid, $title, $text, $url, $type){
-		global $con;
 		$query = "SELECT * FROM user_post LEFT JOIN post ON post.pid = user_post.pid WHERE user_post.uid=".$uid;
-		$result = mysqli_query($con,$query);
+		$result = mysqli_query($this->con,$query);
 
 		if($result){
 			$query = "UPDATE post SET title='".$title."',text='".$url."', post_image='".$url."', type='".$type."' WHERE pid=".$pid;
-			$result2 = mysqli_query($con,$query);
+			$result2 = mysqli_query($this->con,$query);
 			if($result2){
 				return true;
 			}
@@ -171,13 +166,12 @@ class Noni{
 
 	}
 	function del_post($pid,$uid){
-		global $con;
 		$query = "SELECT * FROM user_post LEFT JOIN post ON post.pid = user_post.pid WHERE user_post.uid=".$uid;
-		$result = mysqli_query($con,$query);
+		$result = mysqli_query($this->con,$query);
 		//$query ="DELETE FROM user_post WHERE user_post.pid='".$pid."' AND user_post.uid= ".$uid;
 		if($result){
 			$query2 ="DELETE FROM post WHERE post.pid=".$pid;
-			$result2 = mysqli_query($con,$query2);
+			$result2 = mysqli_query($this->con,$query2);
 			if($result2){
 				return true;
 			}
@@ -186,13 +180,12 @@ class Noni{
 		return false;
 	}
 	function edit_comment($pid, $uid, $cid, $comment){
-		global $con;
 		$query = "SELECT * FROM comments_posts LEFT JOIN comments ON comments.cid = comments_posts.cid LEFT JOIN post ON comments_posts.pid=post.pid WHERE comments_posts.pid='".$pid."' AND comments_posts.cid='".$cid."' AND comments_posts.uid='".$uid."'";
-		$result = mysqli_query($con,$query);
+		$result = mysqli_query($this->con,$query);
 
 		if($result){
 			$query = "UPDATE comments SET comment='".$comment."'WHERE cid=".$cid;
-			$result2 = mysqli_query($con,$query);
+			$result2 = mysqli_query($this->con,$query);
 			if($result2){
 				return true;
 			}
@@ -201,12 +194,11 @@ class Noni{
 		return false;
 	}
 	function del_comment($cid,$uid){
-		global $con;
 		$query = "SELECT * FROM comments_posts LEFT JOIN comments ON comments.cid = comments_posts.cid LEFT JOIN post ON comments_posts.pid=post.pid WHERE comments_posts.uid='".$uid."' AND comments_posts.cid='".$cid."'";
-		$result = mysqli_query($con,$query);
+		$result = mysqli_query($this->con,$query);
 		if($result){
 			$query2 ="DELETE FROM comments WHERE comments.cid=".$cid;
-			$result2 = mysqli_query($con,$query2);
+			$result2 = mysqli_query($this->con,$query2);
 			if($result2){
 				return true;
 			}
