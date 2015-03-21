10405430f425d70b6fa8a8c1bdd0ff006ed24a0b
annelorraineuy <annelorraineuy@gmail.com>
2015-03-21 15:54:14 -0700

    avoid voting more than once

--- view/index.php
+++ view/index.php
@@ -72,7 +72,6 @@
 						cardType= "<div class='card-image'><a href='post.php?pid="+resp.pid+"'><img src='"+resp.post_text+"' class='post-image'/><span class='card-title'>"+resp.post_title+"</span></a></div>";
 						card = "<div class='card'>"+cardType+"<div class='card-action'><div class='voteBox'><a href='#' class='userVote' data-votetype='0' data-pid='"+resp.pid+"' data-uid='<?php echo $userId_session; ?>'><i class='mdi-hardware-keyboard-arrow-up'></i></a><div class='voteCount' id='voteBox"+resp.pid+"'>0</div><a href='#' data-votetype='1' class='userVote' data-pid='"+resp.pid+"' data-uid='<?php echo $userId_session; ?>'><i class='mdi-hardware-keyboard-arrow-down'></i></a></div><div class='postDetails'><a href='post.php?pid="+resp.pid+"'><i class='mdi-communication-forum'></i> "+resp.num_comment+"</a> <a href='#'><i class='mdi-action-grade'></i>0</a>submitted by <span class='username'><a href='#'>"+resp.username+"</a></span></div></div></div>";
 						
-						votingFunc();
 						$("#card"+resp.pid).append(card);
 						
 					}, function(resp){
@@ -85,7 +84,8 @@
 				else if (postType == 1)
 				{
 					//post type 1 = title and text
-					var card = "<div class='card'><div class='card-content'><span class='card-title'><a href='post.php?pid="+pid+"' class='post-link'>"+post[i].post_title+"</a></span><p>"+post[i].post_text+"</p></div><div class='card-action'><div class='voteBox'><a href='#' class='userVote' data-votetype='0' data-pid='"+post[i].pid+"' data-uid='<?php echo $userId_session; ?>'><i class='mdi-hardware-keyboard-arrow-up'></i></a><div class='voteCount' id='voteBox"+pid+"'>0</div><a href='#' data-votetype='1' class='userVote' data-pid='"+post[i].pid+"' data-uid='<?php echo $userId_session; ?>'><i class='mdi-hardware-keyboard-arrow-down'></i></a></div><div class='postDetails'><a href='post.php?pid="+post[i].pid+"'><i class='mdi-communication-forum'></i> "+post[i].num_comment+"</a> <a href='#'><i class='mdi-action-grade'></i>0</a>submitted by <span class='username'><a href='#'>"+post[i].username+"</a></span></div></div></div>";
+					var card = "<div class='card'><div class='card-content'><span class='card-title'><a href='post.php?pid="+pid+"' class='post-link'>"+post[i].post_title+"</a></span><p>"+post[i].post_text+"</p></div><div class='card-action'><div class='voteBox'><a href='#' data-userVote='none' data-votetype='0' data-pid='"+post[i].pid+"' data-uid='<?php echo $userId_session; ?>'><i class='mdi-hardware-keyboard-arrow-up'></i></a><div class='voteCount' id='voteBox"+pid+"'>0</div><a href='#' data-votetype='1' class='userVote' data-pid='"+post[i].pid+"' data-uid='<?php echo $userId_session; ?>'><i class='mdi-hardware-keyboard-arrow-down'></i></a></div><div class='postDetails'><a href='post.php?pid="+post[i].pid+"'><i class='mdi-communication-forum'></i> "+post[i].num_comment+"</a> <a href='#'><i class='mdi-action-grade'></i>0</a>submitted by <span class='username'><a href='#'>"+post[i].username+"</a></span></div></div></div>";
+					
 				}
 				
 				$("#card"+post[i].pid).append(card);	
@@ -97,6 +97,9 @@
 			  console.log(err);
 			  console.log('fail');
 			}
+		  }).done(function() {
+			  	//when all cards are present bind event listener for votes
+				votingFunc();  
 		  });
 
 		  
@@ -112,15 +115,12 @@ function IsValidImageUrl(url, callback, error) {
 }
 
 function getVoteCount(pid) {
-    console.log(pid);
 	$.ajax({
 		url:'../controller/listener.php',
 		data: {phase: 3, pid: pid},
 		type: 'GET',
 		dataType: 'json',
 		success: function (voteNum) {
-			console.log("inside function "+ pid);
-			console.log(voteNum);
 			$("#voteBox"+pid).text(voteNum);
 		}, 
 		error: function(err) {
@@ -133,12 +133,30 @@ function votingFunc() {
 	//vote click event listener binds dynamically made cards
 	$(document).on("click", ".userVote", function(e) {
 			e.preventDefault();
+			var votetype = $(this).data('votetype');
+			var pid = $(this).data('pid');
+			var currVoteCount = parseInt($("#voteBox"+pid).text());
+			var uservotetype = $(this).data('uservote');
+			console.log($(this).data());
+			
+			if (!votetype)
+			{
+				$("#voteBox"+pid).text(currVoteCount	+1);
+				$(this).attr("class", "disableVote");
+				$(this).siblings().closest("a").attr("class","userVote");
+			}
+			else {
+				$("#voteBox"+pid).text(currVoteCount-1);
+				$(this).attr("class", "disableVote");
+				$(this).siblings().closest("a").attr("class","userVote");
+			}
+			$(document).on("click", ".disableVote", function(e) { e.preventDefault(); });
 			
 			var formData = {
 				phase: 6, 
 				uid: <?php echo $userId_session; ?>, //user in session
-				pid: $(this).data('pid'), 
-				votetype: $(this).data('votetype') 
+				pid: pid, 
+				votetype: votetype 
 			};
 			//0 is upvote, 1 is minus
 			$.ajax({
