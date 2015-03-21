e429516477a1d5d73e6be44ccde5455db6ffb2a3
annelorraineuy <annelorraineuy@gmail.com>
2015-03-21 14:23:55 -0700

    voting done

--- view/index.php
+++ view/index.php
@@ -73,14 +73,12 @@
 						card = "<div class='card'>"+cardType+"<div class='card-action'><div class='voteBox'><a href='#' class='userVote' data-votetype='0' data-pid='"+resp.pid+"' data-uid='<?php echo $userId_session; ?>'><i class='mdi-hardware-keyboard-arrow-up'></i></a><div class='voteCount' id='voteBox"+resp.pid+"'>0</div><a href='#' data-votetype='1' class='userVote' data-pid='"+resp.pid+"' data-uid='<?php echo $userId_session; ?>'><i class='mdi-hardware-keyboard-arrow-down'></i></a></div><div class='postDetails'><a href='post.php?pid="+resp.pid+"'><i class='mdi-communication-forum'></i> "+resp.num_comment+"</a> <a href='#'><i class='mdi-action-grade'></i>0</a>submitted by <span class='username'><a href='#'>"+resp.username+"</a></span></div></div></div>";
 						
 						votingFunc();
-						
 						$("#card"+resp.pid).append(card);
-						//alert(card);
+						
 					}, function(resp){
 						cardType = "<div class='card-content'><span class='card-title'><a href='"+resp.post_text+"' target='_blank'>"+resp.post_title+"</a></span></div>";
 						card = "<div class='card'>"+cardType+"<div class='card-action'><div class='voteBox'><a href='#' class='userVote' data-votetype='0' data-pid='"+resp.pid+"' data-uid='<?php echo $userId_session; ?>'><i class='mdi-hardware-keyboard-arrow-up'></i></a><div class='voteCount' id='voteBox"+resp.pid+"'>0</div><a href='#' data-votetype='1' class='userVote' data-pid='"+resp.pid+"' data-uid='<?php echo $userId_session; ?>'><i class='mdi-hardware-keyboard-arrow-down'></i></a></div><div class='postDetails'><a href='post.php?pid="+pid+"'><i class='mdi-communication-forum'></i> "+resp.num_comment+"</a> <a href='#'><i class='mdi-action-grade'></i>0</a>submitted by <span class='username'><a href='#'>"+resp.username+"</a></span></div></div></div>";
 						$("#card"+resp.pid).append(card);
-						//alert(card);
 					});		
 					
 				}
@@ -90,22 +88,8 @@
 					var card = "<div class='card'><div class='card-content'><span class='card-title'><a href='post.php?pid="+pid+"' class='post-link'>"+post[i].post_title+"</a></span><p>"+post[i].post_text+"</p></div><div class='card-action'><div class='voteBox'><a href='#' class='userVote' data-votetype='0' data-pid='"+post[i].pid+"' data-uid='<?php echo $userId_session; ?>'><i class='mdi-hardware-keyboard-arrow-up'></i></a><div class='voteCount' id='voteBox"+pid+"'>0</div><a href='#' data-votetype='1' class='userVote' data-pid='"+post[i].pid+"' data-uid='<?php echo $userId_session; ?>'><i class='mdi-hardware-keyboard-arrow-down'></i></a></div><div class='postDetails'><a href='post.php?pid="+post[i].pid+"'><i class='mdi-communication-forum'></i> "+post[i].num_comment+"</a> <a href='#'><i class='mdi-action-grade'></i>0</a>submitted by <span class='username'><a href='#'>"+post[i].username+"</a></span></div></div></div>";
 				}
 				
-				$.ajax({
-							url:'../controller/listener.php',
-							data: {phase: 3, pid: post[i].pid},
-							type: 'GET',
-							dataType: 'json',
-							success: function (voteNum) {
-								console.log(post[i].pid);
-								console.log(voteNum);
-								$("#voteBox"+post[i].pid).text(voteNum);
-							}, 
-							error: function(err) {
-								error("Voting Error: "+err);
-							}
-						});
-				
 				$("#card"+post[i].pid).append(card);	
+				getVoteCount(post[i].pid);
 			
 			  }
 			},
@@ -127,25 +111,26 @@ function IsValidImageUrl(url, callback, error) {
     });
 }
 
-function getVoteCount(pid, callback, error) {
+function getVoteCount(pid) {
     console.log(pid);
-	var voteCount;
 	$.ajax({
 		url:'../controller/listener.php',
 		data: {phase: 3, pid: pid},
 		type: 'GET',
 		dataType: 'json',
 		success: function (voteNum) {
-			voteCount = voteNum;
+			console.log("inside function "+ pid);
+			console.log(voteNum);
+			$("#voteBox"+pid).text(voteNum);
 		}, 
 		error: function(err) {
-			error("Voting Error: "+err);
+			console.log(err);
 		}
-	});
+	});  
 }
 
 function votingFunc() {
-	//voting
+	//vote click event listener binds dynamically made cards
 	$(document).on("click", ".userVote", function(e) {
 			e.preventDefault();
 			
