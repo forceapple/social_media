00cc2e985a23b438868f8715008932fa7d938010
annelorraineuy <annelorraineuy@gmail.com>
2015-03-20 17:27:46 -0700

    edited post types

--- view/index.php
+++ view/index.php
@@ -44,6 +44,7 @@
 			data: {phase: 0},
 			type: 'GET',
 			dataType: 'json',
+			async: false,
 			beforeSend: function(){
 				$('#post-loading').show();
 			},
@@ -55,26 +56,31 @@
 			  {
 				var pid = parseInt(post[i].pid);
 				var postType = post[i].post_type;
+				var cardType, card;
 				
 				//determine post type
+				//post type 0 = text and link or image only
 				if(postType == 0) 
 				{
 					//check if image
-					var cardType= "<div class='card-image'><a href='post.php?pid="+post[i].pid+"'><img src='"+post[i].post_text+"' class='post-image'/><span class='card-title'>"+post[i].post_title+"</span></a></div>";
-					var testImage = "<img src='"+post[i].post_text+"' class='post-image' />";
+					IsValidImageUrl(post[i], function(resp){
+						//image
+						cardType= "<div class='card-image'><a href='post.php?pid="+resp.pid+"'><img src='"+resp.post_text+"' class='post-image'/><span class='card-title'>"+resp.post_title+"</span></a></div>";
+						card = "<div class='card'>"+cardType+"<div class='card-action'><a href='#'><i class='mdi-hardware-keyboard-arrow-up'></i></a><div class='vote'>0 votes</div><a href='#'><i class='mdi-hardware-keyboard-arrow-down'></i></a><a href='post.php?pid="+resp.pid+"'><i class='mdi-communication-forum'></i> "+resp.num_comment+"</a> <a href='#'><i class='mdi-action-grade'></i>0</a>submitted by <span class='username'><a href='#'>"+resp.username+"</a></span></div></div>";
+						$("#content").append(card);
+						//alert(card);
+					}, function(resp){
+						cardType = "<div class='card-content'><span class='card-title'><a href='"+resp.post_text+"' target='_blank'>"+resp.post_title+"</a></span></div>";
+						card = "<div class='card'>"+cardType+"<div class='card-action'><a href='#'><i class='mdi-hardware-keyboard-arrow-up'></i></a><div class='vote'>0 votes</div><a href='#'><i class='mdi-hardware-keyboard-arrow-down'></i></a><a href='post.php?pid="+pid+"'><i class='mdi-communication-forum'></i> "+resp.num_comment+"</a> <a href='#'><i class='mdi-action-grade'></i>0</a>submitted by <span class='username'><a href='#'>"+resp.username+"</a></span></div></div>";
+						$("#content").append(card);
+						//alert(card);
+					});		
 					
-					$(testImage).error(function() {
-						//just URL
-						cardType = "<div class='card-content'><span class='card-title'><a href='"+post[i].post_text+"' target='_blank'>"+post[i].text+"</a></span></div>";
-					});
-					
-					//post type 0 = text and link or image only
-					var card = "<div class='card'>"+cardType+"<div class='card-action'><a href='#'><i class='mdi-hardware-keyboard-arrow-up'></i></a><div class='vote'>0 votes</div><a href='#'><i class='mdi-hardware-keyboard-arrow-down'></i></a><a href='post.php?pid="+pid+"'><i class='mdi-communication-forum'></i> "+post[i].num_comment+"</a> <a href='#'><i class='mdi-action-grade'></i>0</a>submitted by <span class='username'><a href='#'>"+post[i].username+"</a></span></div></div>";
 				}
 				else if (postType == 1)
 				{
 					//post type 1 = title and text
-					var card = "<div class='card'><div class='card-content'><span class='card-title'><a href='post.php?pid="+pid+"' class='post-link'>"+post[i].post_title+"</a></span><p>"+post[i].post_text+"</p></div><div class='card-action'><a href='#'><i class='mdi-hardware-keyboard-arrow-up'></i></a><div class='vote'>0 votes</div><a href='#'><i class='mdi-hardware-keyboard-arrow-down'></i></a><a href='post.php'># of comments</a>posted by <span class='username'>"+post[i].username+"</span></div></div>";
+					var card = "<div class='card'><div class='card-content'><span class='card-title'><a href='post.php?pid="+pid+"' class='post-link'>"+post[i].post_title+"</a></span><p>"+post[i].post_text+"</p></div><div class='card-action'><a href='#'><i class='mdi-hardware-keyboard-arrow-up'></i></a><div class='vote'>0 votes</div><a href='#'><i class='mdi-hardware-keyboard-arrow-down'></i></a><a href='post.php?pid="+post[i].pid+"'><i class='mdi-communication-forum'></i> "+post[i].num_comment+"</a> <a href='#'><i class='mdi-action-grade'></i>0</a>submitted by <span class='username'><a href='#'>"+post[i].username+"</a></span></div></div>";
 				}
 				$("#content").append(card);	
 			
@@ -92,6 +98,16 @@
 
 		  
     });
+	
+function IsValidImageUrl(url, callback, error) {
+    console.log(url.post_text);
+	$("<img>", {
+        src: url.post_text,
+		async: false,
+        error: function() { error(url); },
+        load: function() { callback(url); }
+    });
+}
 </script>
 </body>
 </html>
