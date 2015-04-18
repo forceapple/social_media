34b610687840275b00dca5f7f28e98ec7f97b202
forceapple <life147@live.com>
2015-04-18 12:26:15 -0800

    added login in login.php as it's own class

--- model/login.php
+++ model/login.php
@@ -1,9 +1,41 @@
 <?php
 require ("db.php");
 
-class login{
+// $a="test";
+// $b="1234";
+class login_model{
+
+	private $con;
+	function __construct(){
+		
+		global $con;
+		$this->con = $con;
+	}
+
+
+	function login($username, $pass){
+		$query = "SELECT user.username, user.f_name, user.l_name, user.profile_img FROM user WHERE user.username='".$username."' AND user.password=".$pass;
+		$result = mysqli_query($this->con, $query);
+		$arr= array();
+		if($result){
+			while($row = mysqli_fetch_array($result)){
+				$arr['username']= $row['username'];
+				$arr['f_name']= $row['f_name'];
+				$arr['l_name']= $row['l_name'];
+				$arr['profile_img']= $row['profile_img'];
+				
+			}	
+			print_r($arr);
+			return $arr;
+		}
+		return false;
+	}
+	
 
 
 }
+
+// $db = new login_model();
+// $db->login($a,$b);
 
 ?>
