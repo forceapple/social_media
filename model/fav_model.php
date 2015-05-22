b6059addaf5203ebcf9b06ecca0c869f578c805a
forceapple <life147@live.com>
2015-05-21 21:31:26 -0700

    get all fav by uid

--- model/fav_model.php
+++ model/fav_model.php
@@ -1,5 +1,5 @@
 <?php
-//require ("_model_interface.php");
+require ("_model_interface.php");
 class fav_model extends _Model_Interface{
 	function __construct(){
 		// parent::__construct("user"); //passing the table
@@ -16,12 +16,24 @@ class fav_model extends _Model_Interface{
 	}
 
 	function get_all_fav_by_uid($uid){
-
+		$query = "SELECT * FROM fav LEFT JOIN post ON post.pid = fav.pid WHERE fav.uid=".$uid;
+		$result = $this->result($query);
+		if($result){
+			$arr = array();
+			while($row = mysqli_fetch_array($result)){	
+				$arr['post_title']= $row['title'];
+				$arr['pid']= $row['pid'];
+				$arr['text']=$row['text'];
+				$arr['uid']=$row['uid'];
+				$arr2[]=$arr;
+			}
+		}
+		print_r($arr2);
 	}
 }
-// $a=16;
+$a=2;
 // $b=6;
-//  $db = new fav_model();
-//  $db->fav_post($a, $b);
+ $db = new fav_model();
+ $db->get_all_fav_by_uid($a);
 
 ?>
